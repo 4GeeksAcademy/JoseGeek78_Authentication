154ea4a127e02fae6892ae3336caafb0193711c0
Jose <126101249+JoseGeek78@users.noreply.github.com>
2023-12-21 18:32:35 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,31 +5,45 @@ import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
+from flask_cors import CORS
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db, User
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 
-# from models import Person
+from flask_bcrypt import Bcrypt
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+#from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
-static_file_dir = os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), '../public/')
+static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+bcrypt = Bcrypt(app)
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
-        "postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-MIGRATE = Migrate(app, db, compare_type=True)
+MIGRATE = Migrate(app, db, compare_type = True)
 db.init_app(app)
+
+# Allow CORS requests to this API
+CORS(app)
 
 # add the admin
 setup_admin(app)
@@ -41,15 +55,11 @@ setup_commands(app)
 app.register_blueprint(api, url_prefix='/api')
 
 # Handle/serialize errors like a JSON object
-
-
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
-
-
 @app.route('/')
 def sitemap():
     if ENV == "development":
@@ -57,16 +67,59 @@ def sitemap():
     return send_from_directory(static_file_dir, 'index.html')
 
 # any other endpoint will try to serve it like a static file
-
-
 @app.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
         path = 'index.html'
     response = send_from_directory(static_file_dir, path)
-    response.cache_control.max_age = 0  # avoid cache memory
+    response.cache_control.max_age = 0 # avoid cache memory
     return response
 
+# FORM ENDPOINTS
+
+@app.route('/signup', methods=['POST'])
+def handle_new_user():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg':'Body must be filled'}), 400
+    if 'email' not in body or body['email'] is None or body['email'] == '':
+        return jsonify({'msg': 'Specify email'}), 400
+    if User.query.filter_by(email= body['email']).first():
+        return jsonify({'msg': 'There is already an account associated with that email'}), 400
+    if 'password' not in body or body['password'] is None or body['password'] == '':
+        return jsonify({'msg': 'Specify password'}), 400
+    pw_hash = bcrypt.generate_password_hash(body['password']).decode('utf-8')
+    new_user = User()
+    new_user.email = body['email']
+    new_user.password = pw_hash
+    new_user.is_active = True
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'msg': 'User successfully registered '}), 200
+
+@app.route('/login', methods=['POST'])
+def login():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg':'Body must be filled'}), 400
+    if 'email' not in body  or body['email'] is None or body['email'] == '':
+        return jsonify({'msg': 'Specify email'}), 400
+    if 'password' not in body or body['password'] is None or body['password'] == '': 
+        return jsonify({'msg': 'Specify password'}), 400
+    user = User.query.filter_by(email = body['email']).first()
+    if not user:
+        return jsonify({"msg": "Bad username or password"}), 400
+    if not bcrypt.check_password_hash(user.password, body['password']):
+        return jsonify({"msg": "Bad username or password"}), 400
+    
+    access_token = create_access_token(identity=user.email)
+    return jsonify(access_token=access_token)
+
+@app.route('/my-account', methods=['GET'])
+@jwt_required()
+def my_account():
+    email = get_jwt_identity()
+    return jsonify(user=email), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
